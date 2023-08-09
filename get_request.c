f4b164dd000fdbbe90044258c860fc4bf52520e9
hoon_notebook <foyt01@naver.com>
2023-08-09 12:56:41 +0900

    1

--- get_request.c
+++ get_request.c
@@ -1,5 +1,6 @@
 #include <errno.h>
 #include "get_request.h"
+#include "unix_wrapper.h"
 
 // 에러 처리 코드 추가 필요
 // req는 요청 메시지를 저장할 구조체를 가리키는 포인터, size는 그 구조체의 크기(sizeof)
@@ -7,18 +8,26 @@ int _get_req(int sd, void * req, int size)
 {
     char * temp = (char *)req;
     while(temp < (char *)req + size){
-        int err = read(sd, temp, 1);
-        if(err<0){
-            if(errno!=EINTR){
-                return -1;
-            }
-        }
-        else if(err==0){ // 요청 메시지를 받는 중 정상 종료가 됐다는 것은 오류 상황.
-            return -1;
-        }
-        else{
-            temp++;
-        }
+        Read(sd, temp, 1);
+        temp++
     }
     return 0;
+}
+
+void _get_req_mcu(int sd, void * req, int type)
+{
+    if(type==MESSAGE_A){
+        ;
+    }
+    else if(type==MESSAGE_B){
+        char double_s_temp[100];
+        char * temp = double_s_temp;
+        while(true){
+            Read(sd, temp, 1);
+            if(*temp == ' '){
+                *temp = '\0';
+            }
+            temp++;
+        } 
+    }
 }
